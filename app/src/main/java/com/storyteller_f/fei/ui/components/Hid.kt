a5bc4895891581bf9b0e6135ea829c452af768cc
storytellerF <34095089+storytellerF@users.noreply.github.com>
2023-03-25 22:08:07 +0800

    ui impr

--- app/src/main/java/com/storyteller_f/fei/ui/components/Hid.kt
+++ app/src/main/java/com/storyteller_f/fei/ui/components/Hid.kt
@@ -5,11 +5,16 @@ import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothHidDevice
 import android.bluetooth.BluetoothHidDeviceAppSdpSettings
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
+import android.provider.Settings
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -18,7 +23,10 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateMapOf
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.app.ActivityCompat
@@ -66,6 +74,11 @@ fun HidScreen(
     connectDevice: (String) -> Boolean,
     sendText: (String) -> Unit = {},
 ) {
+    val context = LocalContext.current
+    val toBluetoothSettings = {
+        val intent = Intent(Settings.ACTION_BLUETOOTH_SETTINGS)
+        context.startActivity(intent)
+    }
     when (bluetoothState) {
         HidState.BluetoothOff -> {
             Text(text = "请打开蓝牙")
@@ -81,10 +94,20 @@ fun HidScreen(
 
         HidState.NoBond -> {
             if (bondDevices.isEmpty()) {
-                Text(text = "没有设备可供连接", modifier = Modifier.padding(8.dp))
+                Column(modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+                    Text(text = "没有设备可供连接", modifier = Modifier.padding(8.dp))
+                    Button(onClick = toBluetoothSettings) {
+                        Text(text = "去配对")
+                    }
+                }
             } else {
                 Column {
                     Text(text = "选择你的设备", modifier = Modifier.padding(8.dp), fontSize = 20.sp)
+                    Button(onClick = toBluetoothSettings) {
+                        Text(text = "找不到预期的设备，去配对")
+                    }
                     LazyColumn {
                         items(bondDevices.size) {
                             BoundDevice(bondDevices[it], connectDevice)
@@ -96,27 +119,35 @@ fun HidScreen(
 
         HidState.Done -> {
             Column {
-                Button(onClick = {
-                    sendText("fei")
-                }) {
-                    Text(text = "测试键盘")
-                }
-                Button(onClick = {
-                    sendText("Z")
-                }) {
-                    Text(text = "Z")
-                }
-                Button(onClick = {
-                    sendText("/")
-                }) {
-                    Text(text = "/")
-                }
-                Button(onClick = {
-                    if (!keyboardInterceptor.contains(Dvorak)) {
-                        keyboardInterceptor.add(Dvorak)
+                Row {
+                    Button(onClick = {
+                        sendText("fei")
+                    }) {
+                        Text(text = "fei")
                     }
-                }) {
-                    Text(text = "我使用了Dvorak 特殊键盘布局")
+                    Button(onClick = {
+                        sendText("z")
+                    }) {
+                        Text(text = "z")
+                    }
+                    Button(onClick = {
+                        sendText("/")
+                    }) {
+                        Text(text = "/")
+                    }
+                }
+                if (!keyboardInterceptor.contains(KeyboardInterfaceInterceptor.key)) {
+                    Button(onClick = {
+                        keyboardInterceptor.putIfAbsent(KeyboardInterfaceInterceptor.key, Dvorak)
+                    }) {
+                        Text(text = "我使用了Dvorak 特殊键盘布局")
+                    }
+                } else {
+                    Button(onClick = {
+                        keyboardInterceptor.remove(KeyboardInterfaceInterceptor.key)
+                    }) {
+                        Text(text = "移除Dvorak 键盘布局")
+                    }
                 }
             }
         }
@@ -215,12 +246,16 @@ fun Context.sendReport(
 }
 
 fun String.toKeyCode(block: (Pair<Int, Int>) -> Unit) {
-    keyboardInterceptor.fold(this) { s, f ->
+    keyboardInterceptor.values.fold(this) { s, f ->
         f.intercept(s)
     }.forEach {
         when (it) {
             in 'a'..'z' -> {
                 block(it - 'a' + 4 to 0)
+            }
+
+            in 'A' .. 'Z' -> {
+                block(it - 'A' to 2)
             }
 
             in '1'..'9' -> {
@@ -239,7 +274,7 @@ fun String.toKeyCode(block: (Pair<Int, Int>) -> Unit) {
     }
 }
 
-val keyboardInterceptor = mutableListOf<KeyboardInterceptor>()
+val keyboardInterceptor = mutableStateMapOf<String, KeyboardInterceptor>()
 
 const val qwerty = "-=qwertyuiop[]asdfghjkl;'zxcvbnm,./"
 const val dvorak = "']x,doktfgsr-=a;hyujcvpzq/bi.nlmwe["
@@ -248,7 +283,13 @@ interface KeyboardInterceptor {
     fun intercept(data: String): String
 }
 
-object Dvorak : KeyboardInterceptor {
+interface KeyboardInterfaceInterceptor : KeyboardInterceptor {
+    companion object {
+        const val key = "keyboard style"
+    }
+}
+
+object Dvorak : KeyboardInterfaceInterceptor {
     override fun intercept(data: String): String {
         assert(qwerty.length == dvorak.length)
         return data.map {
