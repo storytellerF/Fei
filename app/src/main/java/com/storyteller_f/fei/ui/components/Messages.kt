390e2190ccbd0a420540b46d1d7fa04d6b76846d
storytellerF <34095089+storytellerF@users.noreply.github.com>
2024-06-20 07:30:50 +0800

    支持编辑markdown 消息

--- app/src/main/java/com/storyteller_f/fei/ui/components/Messages.kt
+++ app/src/main/java/com/storyteller_f/fei/ui/components/Messages.kt
@@ -3,6 +3,7 @@ package com.storyteller_f.fei.ui.components
 import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,13 +12,16 @@ import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
@@ -32,10 +36,12 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.rememberTextMeasurer
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
+import androidx.window.core.layout.WindowWidthSizeClass
 import coil.compose.AsyncImage
 import com.storyteller_f.fei.R
 import com.storyteller_f.fei.service.Message
@@ -115,40 +121,93 @@ class MessageContentProvider : PreviewParameterProvider<List<Message>> {
 
 }
 
-@Preview
+@Preview(device = Devices.TABLET)
+@Preview(device = Devices.PHONE)
 @Composable
 fun MessagePage(
     @PreviewParameter(MessageContentProvider::class) messageList: List<Message>,
+    initMessage: String = "",
     sendMessage: (String) -> Unit = {}
 ) {
     var content by remember {
-        mutableStateOf("")
+        mutableStateOf(initMessage)
     }
-    Column {
-        LazyColumn(
-            modifier = Modifier
-                .padding(20.dp)
-                .weight(1f),
-        ) {
-            items(messageList.size) {
-                val message = messageList[it]
-                MessageItem(item = message)
+    val scrollState = rememberScrollState()
+    val windowSizeClass = currentWindowAdaptiveInfo().windowSizeClass
+    if (windowSizeClass.windowWidthSizeClass == WindowWidthSizeClass.COMPACT) {
+        Column(modifier = Modifier.padding(20.dp)) {
+            Box(
+                modifier = Modifier
+                    .weight(1f),
+            ) {
+                LazyColumn(modifier = Modifier.padding(bottom = 8.dp)) {
+                    items(messageList.size) {
+                        MessageItem(item = messageList[it])
+                    }
+                }
+                MarkdownText(
+                    markdown = content,
+                    modifier = Modifier
+                        .align(Alignment.BottomCenter)
+                        .fillMaxWidth()
+                        .background(MaterialTheme.colorScheme.background)
+                        .padding(8.dp)
+                        .verticalScroll(scrollState)
+                )
+            }
+
+            InputGroup(content = content, onValueChange = {
+                content = it
+            }, sendMessage = sendMessage)
+        }
+
+    } else {
+        Row(modifier = Modifier.padding(20.dp)) {
+            LazyColumn(
+                modifier = Modifier
+                    .padding(bottom = 8.dp)
+                    .weight(1f)
+            ) {
+                items(messageList.size) {
+                    MessageItem(item = messageList[it])
+                }
+            }
+            Column(modifier = Modifier.weight(1f)) {
+                MarkdownText(
+                    markdown = content,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(MaterialTheme.colorScheme.background)
+                        .padding(8.dp)
+                        .verticalScroll(scrollState)
+                        .weight(1f)
+                )
+                InputGroup(content = content, onValueChange = {
+                    content = it
+                }, sendMessage = sendMessage)
             }
         }
-        Row(modifier = Modifier.padding(8.dp), verticalAlignment = Alignment.CenterVertically) {
-            TextField(
-                value = content, onValueChange = {
-                    content = it
-                }, modifier = Modifier
-                    .weight(1f)
-                    .heightIn(max = 100.dp)
-            )
-            Button(onClick = {
-                sendMessage(content)
-                content = ""
-            }, modifier = Modifier.padding(start = 8.dp)) {
-                Text(text = stringResource(R.string.send))
-            }
+    }
+}
+
+@Composable
+fun InputGroup(content: String, onValueChange: (String) -> Unit, sendMessage: (String) -> Unit) {
+    Row(
+        modifier = Modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        TextField(
+            value = content, onValueChange = {
+                onValueChange(it)
+            }, modifier = Modifier
+                .weight(1f)
+                .heightIn(max = 100.dp)
+        )
+        Button(onClick = {
+            sendMessage(content)
+            onValueChange("")
+        }, modifier = Modifier.padding(start = 8.dp)) {
+            Text(text = stringResource(R.string.send))
         }
     }
 }
